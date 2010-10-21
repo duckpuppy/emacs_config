77da4dec58e7007d6e6bf7632a8cd97e82c52cef
Patrick Aikens <paikens@gmail.com>
2010-10-21 14:08:09 -0400

    Changed java config to use malabar 1.4.0

--- site-lisp/config/java.el
+++ site-lisp/config/java.el
@@ -1,5 +1,4 @@
-(add-local-load-path "malabar-1.5-SNAPSHOT/lisp")
+(add-local-load-path "malabar-1.4.0/lisp")
 (require 'malabar-mode)
-(setq malabar-groovy-lib-dir "~/.emacs.d/site-lisp/malabar-1.5-SNAPSHOT/lib")
+(setq malabar-groovy-lib-dir "~/.emacs.d/site-lisp/malabar-1.4.0/lib")
 (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
-
