feab77fc1337cd4c020659bec3b31d5492af52ea
Patrick Aikens <paikens@gmail.com>
2012-05-25 10:54:00 -0400

    Remove p4 and add it to site-lisp

--- paikens-dt.el
+++ paikens-dt.el
@@ -1,10 +1,3 @@
-;; Add in your own as you wish:
-(defvar my-paikens-dt-packages '(p4))
-
-(dolist (p my-paikens-dt-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
 ;; Fix flyspell
 (setq ispell-program-name "C:/Program Files (x86)/Aspell/bin/aspell.exe")
 
